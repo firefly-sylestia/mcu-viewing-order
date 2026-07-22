b72d294055e8814799f29323d4ff75af88273e88
firefly-sylestia <khanquadir19@gmail.com>
2026-07-22 14:05:31 +0000

    fix: remove progress param from Videasy URL causing media not found

--- src/utils/mediaProviders.js
+++ src/utils/mediaProviders.js
@@ -12,12 +12,10 @@ export const buildPlayerUrl = ({ provider, mediaType, tmdbId, title, season = 1,
     return `https://video.moviepire.co/embed/${path}?${params.toString()}`;
   }
 
-  // Videasy params per their official docs
-  if (progressSeconds > 5) params.set('progress', String(progressSeconds));
   const path = type === 'tv' && episode
     ? `${type}/${identifier}/${season}/${episode}`
     : `${type}/${identifier}`;
-  return `https://player.videasy.net/${path}?${params.toString()}`;
+  return `https://player.videasy.net/${path}`;
 };
 
 export const buildDownloadUrl = ({ mediaType, tmdbId, season = 1, episode }) => {
